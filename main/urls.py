532848519648a4a08af4bbb2294be6e7d6d05084
blackbrokkoli <kastomat@gmail.com>
2025-02-23 13:58:49 +0100

    minor url and ui flow improvs

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', vocab_list, name='vocab_list'),
     path('add/', vocab_add, name='vocab_add'),
     path('<int:pk>/edit/', vocab_add, name='vocab_edit'),
     path('list/', vocab_list, name='vocab_list'),
