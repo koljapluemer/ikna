6aea0f41b34c5685c28d7954b784e19191c399d4
blackbrokkoli <kastomat@gmail.com>
2025-02-23 13:37:53 +0100

    fix statifiles stuff for heroku

--- ikna/settings.py
+++ ikna/settings.py
@@ -104,7 +104,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Enable WhiteNoise for serving static files
+MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    *MIDDLEWARE,  # Keep the existing middleware stack
+]
+
+# Add this at the bottom of `settings.py`
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
